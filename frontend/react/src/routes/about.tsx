5212b0ec1bd9ff1ae01d80ba88abd71df6f0cbbd
Young <liyang_email@163.com>
2026-07-17 14:38:21 +0800

    feat(frontend): 移除react模版

--- frontend/react/src/routes/about.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import About from "@/page/About";
-
-export const Route = createFileRoute("/about")({
-	component: About,
-});
--- frontend/react/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import Index from "@/page/Index";
-
-export const Route = createFileRoute("/")({
-	component: Index,
-});
